1060390862c984847a40cf70ff2bbc0fc026a97f
iamukn <n.u.kingsley@gmail.com>
2023-03-24 21:58:04 +0000

    creating a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # this creates a file at /tmp
 file {'/tmp/school':
     ensure  => 'file'
-    owner   => 'I love Puppet',
+    owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744'.
     content => 'I love Puppet',
