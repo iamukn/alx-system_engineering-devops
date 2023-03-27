4d9b8da7c6da94d007da966d60b4c6635fec4c35
iamukn <n.u.kingsley@gmail.com>
2023-03-27 15:59:52 +0000

    configuring the sshd_config file using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # puppet script to create ssh config file
 file_line {
     ensure  => 'present',
-    path    => '/etc/ssh/sshd_config',
+    path    => '/etc/ssh/ssh_config',
     line    => 'PasswordAuthentication no'
 }
 
 file_line {
     ensure   => 'present',
-    path     => '/etc/ssh/sshd_config',
+    path     => '/etc/ssh/ssh_config',
     line     => 'IdentityFile ~/.ssh/school',
 }
