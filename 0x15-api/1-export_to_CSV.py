35e0975d8958b264feebda02727f4f50a841e7b3
iamukn <n.u.kingsley@gmail.com>
2023-04-26 21:01:21 +0000

    export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,27 +13,15 @@ if __name__ == '__main__':
     response = requests.get(url)
     employeeName = response.json().get('name')
     employeeUsername = response.json().get('username')
+
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
     tasks = response.json()
-    done = 0
-    done_tasks = []
 
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
-
-    with open(f"{employeeId}.csv", 'w') as f:
+    with open('{}.csv'.format(employeeId), 'w') as file:
         """ Opening a new file using the ID as the name.CSV"""
 
         for task in tasks:
-            f.write('"{}","{}","{}","{}"\n'.format(employeeId,
+            file.write('"{}","{}","{}","{}"\n'.format(employeeId,
                     employeeUsername, task.get('completed'),
                     task.get('title')))
