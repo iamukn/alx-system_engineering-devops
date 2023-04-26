0dbcf7fe03e5ce5490e443f2fbff455485fc89b5
iamukn <n.u.kingsley@gmail.com>
2023-04-26 22:06:35 +0000

    export to Json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Accessing REST API for todo lists of employees"""
 
+import json
 import requests
 import sys
-import json
 
 if __name__ == '__main__':
     employeeId = sys.argv[1]
