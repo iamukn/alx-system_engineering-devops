3c159ebd672c10d81203e02ce6bfe0e3c8bf098c
iamukn <n.u.kingsley@gmail.com>
2023-05-10 19:08:38 +0000

    total subscribers on reddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from requests import get
+
+"""Making a get request to the Reddit APi and
+printing the total subscriber to the console"""
+
+
+def number_of_subscribers(subreddit):
+    """receives a subreddit and returns the total
+    subscribers on reddit"""
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
+    res = get(url, user_agent)
+
+    data = res.json()
+
+    total_subscribers = data.get('data').get('subscribers')
+
+    return total_subscribers
