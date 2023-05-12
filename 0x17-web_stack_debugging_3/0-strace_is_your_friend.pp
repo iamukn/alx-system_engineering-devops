561147bed3b4961556e22d9f3584f9909adc15c2
iamukn <n.u.kingsley@gmail.com>
2023-05-12 13:16:17 +0000

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,10 @@
-file { '/path/to/directory':
-  ensure  => directory,
-  mode    => '0755',
-  owner   => 'apache',
-  group   => 'apache',
-  recurse => true,
+# A puppet manuscript that replaces a line in a file on a server
+
+$file_to_modify = '/var/www/html/wp-settings.php'
+
+#replace phrase containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_modify}",
+  path    => ['/bin','/usr/bin']
 }
